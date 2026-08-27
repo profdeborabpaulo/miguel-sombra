a47a35416c364f0746a1d0c458a3109dcd7fbd06
miguel-sombra <sombritozz@gmail.com>
2026-08-27 10:49:08 -0300

    Versão 3 do projeto AppTarefas
    Envio da pasta src atualizada

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,50 @@
-import { View, Text, TextInput, StyleSheet }from 'react-native'; 
-export default function HomeScreen () {
-  return(
-    <View>
-      <Text>Gerenciador de Tarefas</Text>
-      <TextInput
-      placeholder="Digite uma tarefa:"
-      />
-    </View>
-  )
+// importando os componentes do react-native
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { useState } from 'react';
 
+export default function HomeScreen( ) {
+  const[tarefa, setTarefa]= useState('');
+  const[tarefas, setTarefas]= useState([
+    "Estudar React-Native",
+    "Aprender useState",
+    "Criar a primeira tela" 
+  ]);
+  function adicionarTarefa() {
+    if(!tarefa.trim())//impede tarefas em branco
+      return;
+      setTarefas([...tarefas, tarefa]);
+      setTarefa('');
+    
+  }
+  //próxima   return(
+    <View style={styles.container }>
+
+      <Text style={styles.title}> Gerenciador de Tarefas </Text>
+      
+      <TextInput style={styles.input}
+      placeholder='Digite uma tarefa'
+      />
+    
+    </View>
+  );
 }
+
+//Configurando o estilo do index.tsx
+const styles=StyleSheet.create({
+  container:{
+    flex: 1,
+    padding: 20,
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input:{
+    borderWidth: 1,
+    borderBlockColor: '#ccc',
+    borderRadius: 8,
+    padding: 12,
+  },
+
+});
